5fd988bb8567e103277bc0975d84f54f2f6a144b
atlaswacera-cyber <atlaswacera@gmail.com>
2026-08-13 11:35:07 -0500

    Set up React project infrastructure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="app">
+      <h1 className="app__title">Visual Heights Media</h1>
+    </main>
+  );
+}
+
+export default App;
