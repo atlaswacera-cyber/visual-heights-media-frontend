9e525af6dd6aab00383f232c185f5f9e32194f0e
atlaswacera-cyber <atlaswacera@gmail.com>
2026-08-24 17:21:33 -0500

    Revert "Merge pull request #1 from atlaswacera-cyber/stage-1-frontend-and-api"
    This reverts commit 836d13ab2277e5f00f282dd9e28aaca4fe53bc9a, reversing
    changes made to 1960004deda7b7e0cde71e60319a05a5a968d880.

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">© 2026 Visual Heights Media</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- src/components/Preloader/Preloader.jsx
+++
@@ -1,7 +0,0 @@
-import "./Preloader.css";
-
-function Preloader() {
-  return <p className="preloader">Loading inspiration…</p>;
-}
-
-export default Preloader;
